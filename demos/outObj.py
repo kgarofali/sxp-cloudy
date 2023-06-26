2d96a418d8233ea65346d40b77e5c3687f1a7a27
Kristen Garofali <kgarofali@gmail.com>
2023-06-26 17:09:53 -0400

    line emission files, code demo

---
+++ demos/outObj.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mpl_colors
+from matplotlib import cm as cmx
+
+
+c = 2.9979e18 #ang/s
+lsun = 3.839e33 #erg/s
+planck = 6.626e-27
+pc_to_cm = 3.08568e18
+
+def getColors(vals, cname='CMRmap', minv=0.05, maxv=0.8, cmap=None,
+              set_bad_vals=False, return_cNorm=False,
+              logNorm=False, Ncol=100, return_cmap=False):
+    '''
+    sM = getColors(arr, cname='jet', minv=0.0, maxv=1.0)
+    sM,cNorm = getColors(arr, cmap=cubehelix.cmap(), return_cNorm=True)
+    '''
+    if cmap is None:
+        cmap = plt.get_cmap(cname)
+    new_cmap = mpl_colors.LinearSegmentedColormap.from_list('trunc({0}, {1:.2f}, {2:.2f})'.format(cmap.name, minv, maxv), cmap(np.linspace(minv, maxv, Ncol)))
+    if set_bad_vals:
+        new_cmap.set_bad('white', alpha=1.0)
+    if logNorm:
+        cNorm = mpl_colors.LogNorm(vmin=vals.min(), vmax=vals.max())
+    else:
+        cNorm = mpl_colors.Normalize(vmin=vals.min(), vmax=vals.max())
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=new_cmap)
+    if return_cNorm:
+        return scalarMap, cNorm
+    if return_cmap:
+        color_list = new_cmap(np.linspace(minv, maxv, Ncol))
+        cmap_name = new_cmap.name+str(Ncol)
+        return new_cmap.from_list(cmap_name, color_list, Ncol)
+    else:
+        scalarMap.set_array(vals)
+        return scalarMap
+
+def nColors(n, **kwargs):
+    '''
+    n_colors(50, minv=0.5, maxv=1.0, cname='Blues')
+    '''
+    sM = getColors(np.linspace(0.0, 1.0), **kwargs)
+    colors = [sM.to_rgba(v) for v in np.linspace(0.0, 1.0, n)]
+    return colors
+
+class modObj(object):
+    '''
+    '''
+    def __init__(self, dir_, prefix, parline, **kwargs):
+        '''
+        this needs to be called from other class or given
+        a line from a ".pars" file
+        [0]modnum; [1]logZ; [2]age; [3]logU; [4]logR; [5]logQ
+        '''
+        self.modnum = int(parline[0])
+        self.logZ = parline[1]
+        self.age = parline[2]
+        self.logU = parline[3]
+        self.logR = parline[4]
+        self.logQ = parline[5]
+        self.nH = parline[6]
+        try:
+            self.efrac = parline[7]
+        except IndexError:
+            self.efrac = -1.0
+        try:
+            self.fbhb = parline[8]
+        except IndexError:
+            self.fbhb = 0.0
+
+        self.logq = np.log10((10.0**self.logQ)/(np.pi*4.0*self.nH*(10.0**self.logR)**2.0))
+        self.fl = '{}{}{}'.format(dir_, prefix, self.modnum)
+        self.load_lines()
+        return
+    def add_lines(self, lines):
+        line_info = np.genfromtxt(self.fl+'.lineflux')
+        lam, flu = line_info[:,0], line_info[:,1]
+        for name, wav in list(lines.items()):
+            matchind = np.argmin(np.abs(lam-wav))
+            self.__setattr__(name, flu[matchind])
+        return
+    def load_lines(self, **kwargs):
+        lines = {'Lya':1215.67,
+                 'Ha':6564.723,
+                 'Hb':4862.763,
+                 'HeIIu': 1640.43,
+                 'HeII':4687.024,
+                 'CIIIua': 1906.68,
+                 'CIIIub': 1908.73,
+                 'CIVa': 1548.19,
+                 'CIVb': 1550.78,
+                 'NIIa':6549.959,
+                 'NIIb':6585.369,
+                 'OIIIua': 1660.81,
+                 'OIIIub': 1666.00,
+                 'OIIIb':5008.314,
+                 'OIVi': 258906.4,
+                 'NeIIi': 128137.9,
+                 'NeIIIb':3869.917,
+                 'NeIIIia': 155553.7,
+                 'NeV': 3427.066,
+                 'NeVia': 143269.2,
+                 'SIIa':6718.396,
+                 'SIIb':6732.78,
+                }
+
+        self.lines = dict(names=np.array(list(lines.keys())), wavs=np.array(list(lines.values())))
+
+        line_info = np.genfromtxt(self.fl+'.lineflux')
+        lam, flu = line_info[:,0], line_info[:,1]
+        for name, wav in list(lines.items()):
+            matchind = np.argmin(np.abs(lam-wav))
+            self.__setattr__(name, flu[matchind])
+        def logify(a,b):
+            return np.log10(a/b)
+
+        #
+        self.log_HeII_Hb = logify(self.HeII, self.Hb)
+        self.log_HeIIu_CIII = logify(self.HeIIu, (self.CIIIua + self.CIIIub))
+        #
+        self.log_CIV_OIII = logify((self.CIVa + self.CIVb),
+                                   (self.OIIIub + self.OIIIua))
+        self.log_CIV_CIII = logify((self.CIVa + self.CIVb),
+                                   (self.CIIIua + self.CIIIub))
+        #
+        self.log_NII_Ha = logify(self.NIIa+self.NIIb, self.Ha)
+        self.log_NIIb_Ha = logify(self.NIIb, self.Ha)
+        #
+        self.log_OIIIb_Hb = logify(self.OIIIb, self.Hb)
+        self.log_OIII_CIII = logify((self.OIIIua + self.OIIIub),
+                                    (self.CIIIua + self.CIIIub))
+        self.log_OIVi_NeIIIia = logify(self.OIVi, self.NeIIIia)
+        #
+        self.log_NeV_NeIIIb = logify(self.NeV, self.NeIIIb)
+        self.log_NeIIIia_NeIIi = logify(self.NeIIIia, self.NeIIi)
+        self.log_NeVia_NeIIIia = logify(self.NeVia, self.NeIIIia)
+
+        return
+
+
+class allmods(object):
+    '''
+    mods = outobj.allmods(dir, prefix)
+    '''
+    def __init__(self, dir_, prefix, **kwargs):
+        self.modpars = np.genfromtxt('{}{}.pars'.format(dir_, prefix))
+        self.load_mods(dir_, prefix, **kwargs)
+        self.set_pars()
+        self.set_arrs()
+
+    def load_mods(self, dir_, prefix, **kwargs):
+        mods = []
+        for par in self.modpars:
+            mod = modObj(dir_, prefix, par, **kwargs)
+            mods.append(mod)
+        self.__setattr__('mods', mods)
+        self.__setattr__('nmods', len(mods))
+        return
+    def set_pars(self):
+        self.logZ_vals = np.unique(self.modpars[:,1])
+        self.age_vals = np.unique(self.modpars[:,2])
+        self.logU_vals = np.unique(self.modpars[:,3])
+        self.logR_vals = np.unique(self.modpars[:,4])
+        self.logQ_vals = np.unique(self.modpars[:,5])
+        self.nH_vals = np.unique(self.modpars[:,6])
+        try:
+            self.efrac_vals = np.unique(self.modpars[:,7])
+        except IndexError:
+            self.efrac_vals = np.array([-1.0])
+        try:
+            self.fbhb_vals = np.unique(self.modpars[:,8])
+        except IndexError:
+            self.fbhb_vals = np.array([0.0])
+    def set_arrs(self):
+        iterstrings = ['logZ', 'age', 'logU', 'logR', 'logQ', 'nH',
+                       'efrac','fbhb']
+        for i in iterstrings:
+            vals = np.array([mod.__getattribute__(i) for mod in self.mods])
+            self.__setattr__(i, vals)
+    def add_arrs(self, *args):
+        for item in args:
+            try:
+                vals = np.array([mod.__getattribute__(item) for mod in self.mods])
+                self.__setattr__(item, vals)
+            except AttributeError:
+                continue
+        return
+    def add_lines(self, linedict={}):
+        '''
+        self.add_lines(linedict={'O3':1666.0})
+        '''
+        [mod.add_lines(linedict) for mod in self.mods]
+        return
+    def group_mods(self, xval='logZ', yval='age', zval='NIIb',
+                   const='logU', cval=-2.0, make_cut=False, **kwargs):
+        grid_x = self.__getattribute__(xval+'_vals')
+        grid_y = self.__getattribute__(yval+'_vals')
+        if make_cut:
+            xlims = kwargs.get('xlims', (-1.0, 0.1))
+            ylims = kwargs.get('ylims', (0.0, 10.e6))
+            grid_x = grid_x[(grid_x >= xlims[0]) & (grid_x <= xlims[1])]
+            grid_y = grid_y[(grid_y >= ylims[0]) & (grid_y <= ylims[1])]
+        X, Y = np.meshgrid(grid_x, grid_y)
+        Z = np.zeros_like(X)
+        for index, x in np.ndenumerate(Z):
+            mind = [i for i in range(self.nmods)
+                    if (self.mods[i].__getattribute__(xval) == X[index]
+                        and self.mods[i].__getattribute__(yval) == Y[index]
+                        and self.mods[i].__getattribute__(const) == cval)]
+            try:
+                Z[index] = self.mods[mind[0]].__getattribute__(zval)
+            except AttributeError:
+                print('not a valid attribute.')
+        if xval == 'age':
+            X*=1.0e-6
+        if yval == 'age':
+            Y*=1.0e-6
+        return X,Y,Z
---
+++ demos/plot_grids.py
@@ -0,0 +1,103 @@
+import outObj as ob
+from outObj import nColors, getColors
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import LogNorm, LinearSegmentedColormap
+from matplotlib import cm, gridspec, patches
+import matplotlib.ticker as ticker
+
+mpl.rc('axes', labelsize=22, titlesize=22, linewidth=3)
+mpl.rc('xtick', labelsize=20, direction='in', top=True)
+mpl.rc('xtick.major', size=12, width=3, top=True)
+mpl.rc('xtick.minor', visible=True, size=8, width=2, top=True)
+mpl.rc('ytick', labelsize=20, direction='in', right=True)
+mpl.rc('ytick.major', size=12, width=3, right=True)
+mpl.rc('ytick.minor', visible=True, size=8, width=2, right=True)
+mpl.rc('font', size=22, family='serif', style='normal', variant='normal', stretch='normal', weight='heavy')
+mpl.rc('legend', labelspacing=0.1, handlelength=2, fontsize=16)
+
+# --- basic function for plotting grids ---
+def make_emline_diagnostic(gridP, gridS, xattr, yattr, xlab, ylab, cval):
+
+    '''
+    plots a set of grids for chosen emission line diagnostic
+
+    gridP and gridS are python objects (as below), where gridS is the secondary grid to be plotted (appears as faded lines)
+
+    xattr and yattr are strings specifying the line ratios for the abscissa and ordinate, respectively. Note, these strings must be defined load_lines() in outObj.py following the examples here for this to work properly.
+
+    xlab and ylab are strings for plotting appropriate axis labels.
+
+    cval is the "constant" value for plotting the grid, in this case selected to be age in units of Myr.
+    '''
+
+    fig, ax = plt.subplots(1, 1, figsize=(8,8))
+    ucolors = nColors(len(gridP.logU_vals),
+                      minv = 0.2, maxv = 1.0, cname = 'Blues')
+    zcolors = nColors(len(gridP.logZ_vals),
+                      minv = 0.3, maxv = 1, cname = 'plasma_r')
+
+
+    Xp,Yp,Zpx = gridP.group_mods(zval = xattr, yval = 'logU',
+                                 const = 'age', cval = cval)
+    Xp,Yp,Zpy = gridP.group_mods(zval = yattr, yval = 'logU',
+                               const = 'age', cval = cval)
+    Xs,Ys,Zsx = gridS.group_mods(zval = xattr, yval = 'logU',
+                                   const = 'age', cval = cval)
+    Xs,Ys,Zsy = gridS.group_mods(zval = yattr, yval = 'logU',
+                                 const = 'age', cval = cval)
+
+    for i in range(Xp.shape[0]):
+        # SXP + SSP, lines of constant logU
+        ax.plot(Zpx[i,:], Zpy[i,:], lw=3, color=ucolors[i])
+        # SSP, lines of constant logU
+        ax.plot(Zsx[i,:], Zsy[i,:], '--', lw=3, alpha=0.3, color=ucolors[i])
+    for i in range(Xp.shape[1]):
+        # SXP + SSP, lines of constant logZ
+        ax.plot(Zpx[:,i], Zpy[:,i], lw=3, color=zcolors[i])
+        # SSP, lines of constant logZ
+        ax.plot(Zsx[:,i], Zsy[:,i], '--', lw=3, alpha=0.3, color=zcolors[i])
+
+    ax.plot(Zpx[-1,0], Zpy[-1,0], '*', markersize=18, color='k')
+    ax.annotate(r'{0:.0f} Myr'.format(cval/1.e6), size=24,
+                xy=(0.98, 0.02), xycoords='axes fraction', ha='right', va='bottom')
+    plt.xlabel(xlab, labelpad=7)
+    plt.ylabel(ylab, labelpad=7)
+    plt.tight_layout()
+    plt.show()
+
+# --- read in data files ---
+dir_sxp = '../sxp-ssp/'
+model_prefix = 'ZAU'
+zau_sxp = ob.allmods(dir_sxp, model_prefix)
+dir_ssp = '../ssp/'
+zau_ssp = ob.allmods(dir_ssp, model_prefix)
+
+# --- call plotting function for lines of interest ---
+make_emline_diagnostic(zau_sxp, zau_ssp,
+                       'log_OIII_CIII', 'log_HeIIu_CIII',
+                       r'${\rm log(O~III]}~\lambda1661,6/{\rm C~III]}~\lambda1907,9)$',
+                       r'${\rm log(He~II}~\lambda1640/{\rm C~III]}~\lambda1907,9)$', cval = 20e+6)
+make_emline_diagnostic(zau_sxp, zau_ssp,
+                       'log_NIIb_Ha', 'log_HeII_Hb',
+                       r'${\rm log([N~II]}~\lambda6584/{\rm H}\alpha$)', r'${\rm log(He~II}~\lambda4686/{\rm H}\beta$)',
+                       cval = 20e+6)
+make_emline_diagnostic(zau_sxp, zau_ssp,
+                        'log_OIVi_NeIIIia', 'log_NeIIIia_NeIIi',
+                        r'${\rm log([O~IV]}~25.9\mu{\rm m}/{\rm [Ne~III]}~15.56\mu{\rm m}$)',
+                        r'${\rm log([Ne~III]}~15.56\mu{\rm m}/{\rm [Ne~II]}~12.81\mu{\rm m}$)', cval = 20e+6)
+make_emline_diagnostic(zau_sxp, zau_ssp,
+                       'log_CIV_OIII', 'log_OIII_CIII',
+                       r'${\rm log~(C~IV}~\lambda1548,51/{\rm O~III]}~\lambda1661,6)$',
+                       r'${\rm log(O~III]}~\lambda1661,6/{\rm [C~III]}~\lambda1907,10)$', cval = 20e+6)
+make_emline_diagnostic(zau_sxp, zau_ssp,
+                       'log_NeV_NeIIIb', 'log_OIIIb_Hb',
+                       r'${\rm log~([Ne~V]}~\lambda3426/{\rm [Ne~III]}~\lambda3870)$',
+                       r'${\rm log~([O~III]}~\lambda 5007/{\rm H}\beta)$',
+                       cval = 20e+6)
+make_emline_diagnostic(zau_sxp, zau_ssp,
+                       'log_NeVia_NeIIIia','log_NeIIIia_NeIIi',
+                       r'${\rm log([Ne~V]}~14.3\mu{\rm m}/{\rm [Ne~III]}~15.56\mu{\rm m}$)',
+                       r'${\rm log([Ne~III]}~15.56\mu{\rm m}/{\rm [Ne~II]}~12.81\mu{\rm m}$)', cval = 20e+6)
